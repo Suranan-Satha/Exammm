61b5858fd0db500230270d462fb56432894a6f1b
Suranan-Satha <sherlockharts@gmail.com>
2025-03-11 21:22:16 +0700

    LL

--- Exam/obj/Debug/net9.0-windows/Exam.AssemblyInfo.cs
+++ Exam/obj/Debug/net9.0-windows/Exam.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd9493d6cd0a8dabcd0b8f9133440f1709f0895")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ea0a1fae19adda6f48844d95b54ba84d14f88a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
